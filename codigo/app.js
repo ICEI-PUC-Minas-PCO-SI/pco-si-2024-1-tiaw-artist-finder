f79ba379484b31fa2b4b794c08f78c991ca1dac6
Pablo Marques Cordeiro <pablomarquescordeiro14@gmail.com>
2024-05-10 21:04:55 -0300

    filtros

--- codigo/app.js
+++ codigo/app.js
@@ -2,77 +2,400 @@
 URL = 'http://localhost:3000/usuarios'
 
 const dbMock = {
-    //Dados de perfil do usuario
-    usuarios: [{
-
-        id: 1,
-        nome: "Pablo Marques",
-        idade: 22,
-        foto: "assets/img/perfil.jpg",
-        estado: "SP",
-        instituicao: "PUC MINAS",
-        Instagram: "",
-        Linkedin: "",
-        disponibilidade: "Freelance",
-        idAtuacao: 4
-    },
-    {
-        id: 2,
-        nome: "Jorge Alves",
-        idade: 29,
-        foto: "assets/img/perfil.jpg",
-        estado: "SP",
-        instituicao: null,
-        Instagram: "",
-        Linkedin: "",
-        disponibilidade: "Todas"
-        ,
-        idAtuacao: 3
-    },
-    {
-        id: 3,
-        nome: "Francisco Xavier",
-        idade: 31,
-        foto: "assets/img/perfil.jpg",
-        estado: "MG",
-        instituicao: "UFRJ",
-        Instagram: "",
-        Linkedin: "",
-        disponibilidade: "Integral"
-        ,
-        idAtuacao: 1
-    },
-    {
-        id: 4,
-        nome: "André Schmidt",
-        idade: 27,
-        foto: "assets/img/perfil.jpg",
-        estado: "RJ",
-        instituicao: null,
-        Instagram: "",
-        Linkedin: "",
-        disponibilidade: "Todas"
-
-        ,
-        idAtuacao: 4
-    }],
-    // Dados de onde meu usuario atua.
-    atuacao: [
+    "usuarios": [
         {
-            id: 1,
-            descricao: "Ilustrador"
+            "id": 1,
+            "nome": "Ana Silva",
+            "idade": 22,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": "PUC MINAS",
+            "disponibilidade": "Freelance",
+            "idAtuacao": 4
         },
         {
-            id: 2,
-            descricao: "Filmmaker",
+            "id": 2,
+            "nome": "Jorge Alves",
+            "idade": 29,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 3
+            },
+        {
+            "id": 3,
+            "nome": "Francisco Xavier",
+            "idade": 31,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "MG",
+            "instituicao": "UFRJ",
+            "disponibilidade": "Integral",
+            "idAtuacao": 1
         },
         {
-            id: 3,
-            descricao: "Fotografo"
+            "id": 4,
+            "nome": "André Schmidt",
+            "idade": 27,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RJ",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 42
         },
         {
-            id: 4,
-            descricao: "Web Design"
+            "id": 5,
+            "nome": "Gabriel Oliveira",
+            "idade": 33,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 10
+          },
+          {
+            "id": 6,
+            "nome": "Carla Santos",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "MG",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 8
+          },
+          {
+            "id": 7,
+            "nome": "Lucas Pereira",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 7
+          },
+          {
+            "id": 8,
+            "nome": "Marina Santos",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RJ",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 8
+          },
+          {
+            "id": 9,
+            "nome": "Thiago Oliveira",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RS",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 3
+          },
+          {
+            "id": 10,
+            "nome": "Amanda Costa",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "AM",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 5
+          },
+          {
+            "id": 11,
+            "nome": "Carla Santos",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "MT",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 9
+          },
+          {
+            "id": 12,
+            "nome": "Felipe Souza",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 10
+          },
+          {
+            "id": 13,
+            "nome": "Rafael Lima",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RS",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 2
+          },
+          {
+            "id": 14,
+            "nome": "Camila Fernandes",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "PR",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 4
+          },
+          {
+            "id": 15,
+            "nome": "Gustavo Rodrigues",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 3
+          },
+          {
+            "id": 16,
+            "nome": "Mariana Almeida",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SC",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 7
+          },
+          {
+            "id": 17,
+            "nome": "Bruno Oliveira",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "BA",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 6
+          },
+          {
+            "id": 18,
+            "nome": "Larissa Silva",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 6
+          },
+          {
+            "id": 19,
+            "nome": "Renato Santos",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RJ",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 9
+          },
+          {
+            "id": 20,
+            "nome": "Isabela Costa",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "MG",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 8
+          },
+          {
+            "id": 21,
+            "nome": "Lucas Pereira",
+            "idade": 28,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Integral",
+            "idAtuacao": 3
+          },
+          {
+            "id": 22,
+            "nome": "Mariana Almeida",
+            "idade": 33,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RJ",
+            "instituicao": null,
+            "disponibilidade": "Freelance",
+            "idAtuacao": 2
+          },
+          {
+            "id": 23,
+            "nome": "Thiago Oliveira",
+            "idade": 26,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 4
+          },
+          {
+            "id": 24,
+            "nome": "Amanda Costa",
+            "idade": 22,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "MG",
+            "instituicao": null,
+            "disponibilidade": "Integral",
+            "idAtuacao": 1
+          },
+          {
+            "id": 25,
+            "nome": "Pedro Almeida",
+            "idade": 31,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RJ",
+            "instituicao": null,
+            "disponibilidade": "Freelance",
+            "idAtuacao": 10
+          },
+          {
+            "id": 26,
+            "nome": "Juliana Santos",
+            "idade": 29,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 3
+          },
+          {
+            "id": 27,
+            "nome": "Felipe Souza",
+            "idade": 34,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Integral",
+            "idAtuacao": 7
+          },
+          {
+            "id": 28,
+            "nome": "Carolina Lima",
+            "idade": 23,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "SP",
+            "instituicao": null,
+            "disponibilidade": "Freelance",
+            "idAtuacao": 10
+          },
+          {
+            "id": 29,
+            "nome": "Bruno Oliveira",
+            "idade": 27,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "RJ",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 6
+          },
+          {
+            "id": 30,
+            "nome": "Camila Fernandes",
+            "idade": 35,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "MG",
+            "instituicao": null,
+            "disponibilidade": "Freelance",
+            "idAtuacao": 3
+          },
+          {
+            "id": 31,
+            "nome": "Gustavo Rodrigues",
+            "idade": 30,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "BA",
+            "instituicao": null,
+            "disponibilidade": "Integral",
+            "idAtuacao": 8
+          },
+          {
+            "id": 32,
+            "nome": "Larissa Silva",
+            "idade": 25,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "PE",
+            "instituicao": null,
+            "disponibilidade": "Freelance",
+            "idAtuacao": 4
+          },
+          {
+            "id": 33,
+            "nome": "Renato Santos",
+            "idade": 36,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "CE",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 6
+          },
+          {
+            "id": 34,
+            "nome": "Isabela Costa",
+            "idade": 28,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "BA",
+            "instituicao": null,
+            "disponibilidade": "Todas",
+            "idAtuacao": 2
+          },
+          {
+            "id": 35,
+            "nome": "Thiago Oliveira",
+            "idade": 32,
+            "foto": "assets/img/perfil.jpg",
+            "estado": "PE",
+            "instituicao": null,
+            "disponibilidade": "Freelance",
+            "idAtuacao": 1
+          }
+                    
+          
+    ],
+    "atuacao": [
+        {
+            "id": 1,
+            "descricao": "Ilustração"
+        },
+        {
+            "id": 2,
+            "descricao": "Filmagem"
+        },
+        {
+            "id": 3,
+            "descricao": "Fotografia"
+        },
+        {
+            "id": 4,
+            "descricao": "Web Design"
+        },
+        {
+            "id": 5,
+            "descricao": "Animação"
+        },
+        {
+            "id": 6,
+            "descricao": "Design Gráfico"
+        },
+        {
+            "id": 7,
+            "descricao": "Modelagem 3D"
+        },
+        {
+            "id": 8,
+            "descricao": "Grafite"
+        },
+        {
+            "id": 9,
+            "descricao": "Pintura"
+        },
+        {
+            "id": 10,
+            "descricao": "Edição"
         }
     ]
 }
@@ -99,20 +422,23 @@ function exibeUsers() {
         let user = dbMock.usuarios[i]
         /* se o filtro de atuacao estiver selicionado 0 (ou seja a opcao filtro) vai imprimir todos os usuarios, caso ela n esteja selicionada criei uma condição para que o if
         veja o id que esta selicionado e mostre somente os usuarios que façam parte daquele id de atuação*/
-        if ( ((FILTRO_ATUACAO == 0) || user.idAtuacao == FILTRO_ATUACAO) &&
-            (FILTRO_ESTADO == "") || user.estado == FILTRO_ESTADO) {
-            str += `<div class="card col-md-2 mb-4 me-4 " >
+
+
+        
+        if (((user.idAtuacao == FILTRO_ATUACAO) || (FILTRO_ATUACAO == 0)) &&
+        ((user.estado == FILTRO_ESTADO) || (FILTRO_ESTADO == ""))) {
+            str += `<div class="card col-md-2  mb-4 me-4 " >
         <img src="${user.foto}" class="card-img-top"  id="foto">
         <div class="card-body" >
-          <h5 class="card-title">${user.nome}</h5>
+          <h5 class="card-title ">${user.nome}</h5>
           <p class="card-text text-secondary ">${getDescricaoAtuacao(user.idAtuacao)}</p>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill  " viewBox="0 0 16 16">
           <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
           </svg>
-          <p class="card-text text-secondary   ">${user.estado}</p>
-          <div class="d-grid gap-2 d-md-block pt-5">
-            <button class="btn btn-primary" type="button">Perfil</button>
-            <button class="btn btn-dark"  type="button">Contratar</button>
+          <p class="card-text text-secondary ">${user.estado}</p>
+          <div class=" row">
+            <button class="btn btn-primary  " type="button">Perfil</button>
+            <button class="btn btn-dark  "  type="button">Contratar</button>
             </div>
         </div>
       </div>`
