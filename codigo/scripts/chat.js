65c8789638b8be9a92c27d82617cc589f35b8574
Lucas Fernandes Nascimento <lucas.nascimento.1529159@sga.pucminas.br>
2024-05-21 15:15:23 -0300

    logica com click na div

--- codigo/scripts/chat.js
+++ codigo/scripts/chat.js
@@ -14,7 +14,7 @@ async function fetchAndDisplayUsers() {
         }
 
         const contactsContainer = document.getElementById('chat-contacts');
-        contactsContainer.innerHTML = ''; // Clear existing contacts
+        contactsContainer.innerHTML = ''; // Limpa os contatos existentes
 
         const usuarios = data.filter(user => !user.loggedIn);
         usuarios.forEach(usuario => {
@@ -23,13 +23,10 @@ async function fetchAndDisplayUsers() {
             userDiv.innerHTML = `
                 <img src="${usuario.foto}" alt="${usuario.nome}" class="chat-sidebar-user-photo">
                 <span class="chat-sidebar-user-name">${usuario.nome}</span>
-                <button id="sidebar-contact-btn" class="sidebar-contact-btn"><i class="fas fa-arrow-right"></i></button>
             `;
             contactsContainer.appendChild(userDiv);
 
-            const sidebarContactBtn = userDiv.querySelector('.sidebar-contact-btn');
-            sidebarContactBtn.addEventListener('click', (event) => {
-                event.stopPropagation();
+            userDiv.addEventListener('click', () => {
                 console.log("Usuário selecionado:", usuario.nome);
                 console.log("Foto do usuário:", usuario.foto);
                 // Aqui você pode adicionar a lógica para atualizar o chat header com os dados do usuário selecionado
