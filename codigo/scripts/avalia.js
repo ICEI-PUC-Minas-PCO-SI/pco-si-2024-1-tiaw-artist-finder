89d10fe3b45595517b1b3b13acd897990c00237c
João Gabriel Soares da Silva Franco <jgssfranco@sga.pucminas.br>
2024-05-26 14:00:12 -0300

    media de avaliações funcionando perfeitamente, pequena adição de atributo no arquivo json

--- codigo/scripts/avalia.js
+++ codigo/scripts/avalia.js
@@ -5,10 +5,18 @@ fetch(urlApi)
   .then(Response => Response.json())
   .then(data => {
     const labels = data.map(item => item.label);
-    const values = data.map(item => item.value); 
+    const values = data.map(item => item.value);
+    const estrelas = data.map(item => item.estrelas); 
     //Calculando a Média
-    const soma = values.reduce((acc, current) => acc + current, 0);
-    const media =  soma / values.length; //Trocar media por um calculo de media ponderada
+    const media =  MediaPond() / QtdA();
+    //Media ponderada
+    function MediaPond() {
+      let somapond = 0;
+      for (let i = 0; i < values.length; i++) {
+        somapond += values[i] * labels[i];
+      }
+      return somapond;
+    }
     //Calculando a quantidade de avaliações
     function QtdA() {
       let Qtd = 0;
@@ -19,12 +27,12 @@ fetch(urlApi)
     }
 
     
-//GRÁFICO CHART.JS FUNCIONANDO
+//GRÁFICO CHART.JS
 const ctx = document.getElementById('stats-bar-chart').getContext('2d');
       const chart = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: labels,
+          labels: estrelas,
           datasets: [{
             label: 'Quantidade de Avaliações',
             data: values,
