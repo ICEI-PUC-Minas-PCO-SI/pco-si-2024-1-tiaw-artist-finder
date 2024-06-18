44989242b347e53524897dbe9644cac39f19c1c5
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2024-06-18 16:26:36 -0300

    savepoint, necessario ajustar explorar e exibe usuarios

--- codigo/scripts/filtro.js
+++ codigo/scripts/filtro.js
@@ -21,14 +21,12 @@ const searchBar = document.getElementById('searchBar');
 //Definindo um array vazil para os usuarios
 let afUsers = [];
 
- let FILTRO_ATUACAO = "";
- let FILTRO_ESTADO = "";
- let FILTRO_DISPONIBILIDADE = "";
+let FILTRO_ATUACAO = "";
+let FILTRO_ESTADO = "";
+let FILTRO_DISPONIBILIDADE = "";
 
 //Defininco o metodo de input capturado na searchbar e usando o includes para pegar o "valor" digitado e criar um novo array chamado filtred users
 //apenas com o array dos usuarios que tem o "valor" digitado na search bar no nome.
-
-
 
 const loadUsers = async () => {
     try {
@@ -41,35 +39,44 @@ const loadUsers = async () => {
 };
 
 //Usando função map para passar por todos usuarios do json array e imprimir os mesmos no html
+
+// Ajuste de lógica para exibir foto do usuário no local storage, se houver.
 const displayUsers = (users) => {
+    const userList = document.getElementById('user-list');
     const user_list = users
         .map((usuario) => {
+            let userPhoto = usuario.foto;
+            if (!userPhoto) {
+                const userPicData = JSON.parse(localStorage.getItem('userPicData'));
+                if (userPicData && userPicData[usuario.id]) {
+                    userPhoto = userPicData[usuario.id];
+                } else {
+                    userPhoto = 'https://cdn-icons-png.flaticon.com/128/1077/1077114.png';
+                }
+            }
+
             return `
-            <div class="col-md-4 mb-5">
-                        <a href="exibe_user.html?id=${usuario.id}">
-                            <div class="card">
-                                <div class="img1"><img src="${usuario.capa}" alt=""></div>
-                                <div class="img2"><img src="${usuario.foto}" alt=""></div>
-                                <div class="main-text">
-                                    <h2>${usuario.nome}</h2>
-                                    <p><strong>Área de atuação:</strong> ${usuario.atuacao}</p>
-                                    <p class="bi bi-geo-fill"> ${usuario.estado}</p>
-                                    <p><strong>Disponibilidade:</strong> ${usuario.disponibilidade}</p>
-                                </div>
-                                <div class="socials">
-                                    <i class="bi bi-facebook"></i>
-                                    <i class="bi bi-linkedin"></i>
-                                    <i class="bi bi-whatsapp"></i>
-                                    <i class="bi bi-instagram"></i>
-                                </div>
+                <div class="col-md-4 mb-5">
+                    <a href="exibe_user.html?id=${usuario.id}">
+                        <div class="card">
+                            <div class="img1"><img src="${usuario.capa}" alt=""></div>
+                            <div class="img2"><img src="${userPhoto}" alt=""></div>
+                            <div class="main-text">
+                                <h2>${usuario.nome}</h2>
+                                <p><strong>Área de atuação:</strong> ${usuario.atuacao}</p>
+                                <p class="bi bi-geo-fill"> ${usuario.estado}</p>
+                                <p><strong>Disponibilidade:</strong> ${usuario.disponibilidade}</p>
                             </div>
-                        </a>
-                    </div>
-        `;
+                        </div>
+                    </a>
+                </div>
+            `;
         })
         .join('');
+    
     userList.innerHTML = user_list;
 };
+
 
 loadUsers();
 
@@ -83,7 +90,6 @@ catch (e) {
     db = null;
 }
 
-
 if (!db) {
     fetch(URL)
         .then(res => res.json())
@@ -91,12 +97,6 @@ if (!db) {
             localStorage.setItem('dbUsers', JSON.stringify(usuariosData));
         })
 }
-
-
-
-
-
-
 
 // Função onde o evento de opção selecionada no HTML compara com os usuários no JSON server
 function exibeUsuarios() {
@@ -106,6 +106,16 @@ function exibeUsuarios() {
             let lista_usuarios = '';
             for (let i = 0; i < usuariosData.length; i++) {
                 let usuario = usuariosData[i];
+                let userPhoto = usuario.foto;
+                if (!userPhoto) {
+                    const userPicData = JSON.parse(localStorage.getItem('userPicData'));
+                    if (userPicData && userPicData[usuario.id]) {
+                        userPhoto = userPicData[usuario.id];
+                    } else {
+                        userPhoto = 'link_para_imagem_padrao.jpg';
+                    }
+                }
+
                 if (((usuario.disponibilidade === FILTRO_DISPONIBILIDADE) || (FILTRO_DISPONIBILIDADE === "")) &&
                     ((usuario.atuacao === FILTRO_ATUACAO) || (FILTRO_ATUACAO === "")) &&
                     ((usuario.estado === FILTRO_ESTADO) || (FILTRO_ESTADO === ""))) {
@@ -115,110 +125,24 @@ function exibeUsuarios() {
                         <a href="exibe_user.html?id=${usuario.id}">
                             <div class="card">
                                 <div class="img1"><img src="${usuario.capa}" alt=""></div>
-                                <div class="img2"><img src="${usuario.foto}" alt=""></div>
+                                <div class="img2"><img src="${userPhoto}" alt=""></div>
                                 <div class="main-text">
                                     <h2>${usuario.nome}</h2>
                                     <p><strong>Área de atuação:</strong> ${usuario.atuacao}</p>
                                     <p class="bi bi-geo-fill"> ${usuario.estado}</p>
                                     <p><strong>Disponibilidade:</strong> ${usuario.disponibilidade}</p>
-                                </div>
-                                <div class="socials">
-                                    <i class="bi bi-facebook"></i>
-                                    <i class="bi bi-linkedin"></i>
-                                    <i class="bi bi-whatsapp"></i>
-                                    <i class="bi bi-instagram"></i>
+                                    <p><strong>Avaliação:</strong> ${usuario.avaliacao} <i class="fa-solid fa-star"></i></p>
                                 </div>
                             </div>
                         </a>
                     </div>`;
                 }
             }
+            const userList = document.getElementById('user-list'); // Supondo que 'user-list' seja o id do elemento onde os usuários serão exibidos
             userList.innerHTML = lista_usuarios;
         });
 }
 /*------------------------------------------------------------------------------------------------------------------------------------------*/
-
-/*Função onde busca o id do usuario selecionado e cria a pagina de perfil a partir dele*/
-function exibeUser(id) {
-    let user = db.find(usuario => usuario.id == id);
-    if (user) {
-        let listar_usuarios = `
-        <div class="main">
-            <div class="header_wrapper">
-                <header class="capa" id="capa"></header>
-                <div class="cols_container">
-                    <div class="left_col">
-                        <div class="img_container">
-                            <img src="${user.foto}" alt="">
-                            <span></span>
-                        </div>
-                        <h2>${user.nome}</h2>
-                        <p>${user.atuacao}</p>
-                        <p>${user.disponibilidade}</p>
-                        <p class="bi bi-geo-fill">${user.estado}</p>
-                        <p>${user.email}</p>
-
-                        <ul class="about">
-                            <li><span>4,000</span>Followers</li>
-                            <li><span>333</span>Following</li>
-                            <li><span>200,300</span>Attractions</li>
-                        </ul>
-
-                        <div class="content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, soluta, totam dolor aliquam placeat vel, facere possimus officia voluptates nam ut dolores est dolore sed dicta tenetur reiciendis aliquid eius.</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, soluta, totam dolor aliquam placeat vel, facere possimus officia voluptates nam ut dolores est dolore sed dicta tenetur reiciendis aliquid eius.</p>
-
-                            <ul>
-                                <li><i class="fab fa-twitter"></i></li>
-                                <li><i class="fab fa-instagram"></i></li>
-                                <li><i class="fab fa-facebook"></i></li>
-                                <li><i class="fab fa-dribbble"></i></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="right_col">
-                        <nav>
-                            <ul>
-                            <li><a href="#">Galeria</a></li>
-                            <li><a href="#">Grupos</a></li>
-                                <li><a href="#">Sobre</a></li>
-                            </ul>
-                            <button>Follow</button>
-                        </nav>
-                        <p> Selecione uma foto para upload <p>
-                        <div class="photos">
-                        
-                            <label for="picture_input" >
-                                <img id="galeria1" src="${user.galeria1}" alt="Photo">
-                                <input type="file" accept="image/*" onchange="previewFile()"  id="picture_input"  /><br />
-                            </label>
-                            <label for="picture_input2"  >
-                                <img id="galeria2" src="${user.galeria2}" alt="Photo">
-                                <input type="file" class accept="image/*" onchange="previewFile2()"  id="picture_input2"  /><br />
-                            </label>
-                            <label for="picture_input3"  >
-                                <img id="galeria3" src="${user.galeria3}" alt="Photo">
-                                <input type="file" class accept="image/*" onchange="previewFile3()"  id="picture_input3"  /><br />
-                            </label>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <style>
-                .main .header_wrapper header {
-                    width: 100%;
-                    background: url(${user.capa}) no-repeat 50% 20% / cover;
-                    min-height: calc(100px + 15vw);
-                }
-            </style>
-        </div>`;
-        mostrarUser.innerHTML = listar_usuarios;
-    } else {
-        mostrarUser.innerHTML = `<h1>Usuário não encontrado</h1>`;
-    }
-}
 
 // Funções que alteram a imagem no local storage a partir do perfil selicionado
 function alterarImagem(id, imageURI) {
