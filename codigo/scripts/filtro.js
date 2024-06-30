bf8183df370099df53cb1573e91db0e7d28c090c
Pablo Marques Cordeiro <pablomarquescordeiro14@gmail.com>
2024-06-30 14:26:42 -0300

    navbar ajustada

--- codigo/scripts/filtro.js
+++ codigo/scripts/filtro.js
@@ -178,7 +178,4 @@ async function getLoggedInUser() {
     }
 } 
 
-window.addEventListener("scroll", function () {
-    let header = document.querySelector('#header');
-    header.classList.toggle('rolagem', window.scrollY > 0);
-});
+
